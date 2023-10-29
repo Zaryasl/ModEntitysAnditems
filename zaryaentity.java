432ade9d155bf688b5ec9bdf6c2c508da915e85f
Zarya <149230745+Zaryasl@users.noreply.github.com>
2023-10-29 00:13:12 -0400

    Create zaryaentity.java

---
+++ zaryaentity.java
@@ -0,0 +1,8 @@
+package com.example.zaryaentity;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(zaryaentity.MODID)
+public class zaryaentity {
+    public static final String MODID = "zaryaentity";
+}
