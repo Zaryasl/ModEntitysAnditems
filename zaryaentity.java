435c48913de6ffb11165622d728603d15f535c85
Zarya <149230745+Zaryasl@users.noreply.github.com>
2023-10-29 22:54:25 -0400

    Update zaryaentity.java

--- zaryaentity.java
+++ zaryaentity.java
@@ -1,8 +1,16 @@
 package com.example.zaryaentity;
 
+import com.example.zaryaentity.init.inititems;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(zaryaentity.MODID)
 public class zaryaentity {
     public static final String MODID = "zaryaentity";
+    public zaryaentity() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        inititems.ITEMS.register(bus);
+    }
 }
